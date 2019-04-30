ffe8a07ed20b4808a25c7036e727509f25004465
Ashwin Nair <ash1995@gmail.com>
2019-04-30 11:24:35 +0400

    Update run_pan.sh

--- run_pan.sh
+++ run_pan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CUDA_VISIBLE_DEVICES=15 python tools/infer_simple.py \
 			--dataset iiai \
-        		--cfg configs/panet/e2e_panet_R-50-FPN_2x_mask.yaml \
+        		--cfg configs/panet/e2e_panet_R-152-FPN_2x_mask.yaml \
                 	--load_ckpt Outputs/R152_18/model_iiai_res152_c18_step179999.pth \
                		--image_dir test_images2 \
                 	--output_dir test_results3/
